64faa863ec5ccc5afd018a5f750ce03c679ae693
Harsh Vardhan Gautam <87386901+harsh100000@users.noreply.github.com>
2024-07-25 02:08:11 +0530

    Update Create.jsx

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
+
 function Create(){
-  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 
   const [task, setTask] = useState()
   const handleAdd = () =>{
