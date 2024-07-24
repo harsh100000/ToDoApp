347e45d3b9b1109cc607662647a4f83ccbacbef3
Harsh Vardhan Gautam <87386901+harsh100000@users.noreply.github.com>
2024-07-24 23:04:49 +0530

    Update Create.jsx

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -5,7 +5,7 @@ function Create(){
 
   const [task, setTask] = useState()
   const handleAdd = () =>{
-    axios.post('http://localhost:3001/add', {task:task})
+    axios.post('https://todo-backend-harsh.onrender.com/add', {task:task})
     .then(result => {
       location.reload();
     })
