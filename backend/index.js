ddd9568642c5d9d8a3dd6c6991c7236666c5e530
Harsh Vardhan Gautam <87386901+harsh100000@users.noreply.github.com>
2024-07-25 01:39:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,29 @@ const cors = require('cors')
 const TodoModel = require('./Models/Todo')
 
 const app = express()
-app.use(cors())
-app.use(express.json())
-
 const port = process.env.PORT || 3001
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://thanos420786:xuQojPQyuRACkrJA@todo-project-db.4pbexa4.mongodb.net/?retryWrites=true&w=majority&appName=todo-project-db'
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err))
+
+const corsOptions = {
+origin: process.env.FRONTEND_URL || 'http://localhost:3000', // replace with your frontend URL
+optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 
 app.get('/get',(req,res)=>{
     TodoModel.find()
